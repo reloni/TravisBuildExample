1453cb96ba09f765321a5a65c1f1920220abc5c3
Anton Efimenko <reloni@ya.ru>
2016-12-11 21:44:17 +0300

    Initial Commit

---
+++ TravisBuildExample/TravisBuildExample.h
@@ -0,0 +1,19 @@
+//
+//  TravisBuildExample.h
+//  TravisBuildExample
+//
+//  Created by Anton Efimenko on 11.12.16.
+//  Copyright © 2016 Anton Efimenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TravisBuildExample.
+FOUNDATION_EXPORT double TravisBuildExampleVersionNumber;
+
+//! Project version string for TravisBuildExample.
+FOUNDATION_EXPORT const unsigned char TravisBuildExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TravisBuildExample/PublicHeader.h>
+
+
